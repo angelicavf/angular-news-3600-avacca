8d36cddf05ac11d315e7c4729f87bfeffecf0490
angelicavf <angelicamvf@gmail.com>
2022-04-28 20:00:19 -0400

    antes del servicio

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  movieList: any[] = [1, 1, 1, 1, 1]
+
   constructor() { }
 
   ngOnInit(): void {
